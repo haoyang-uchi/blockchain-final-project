11c15e90b1acfc8d669536bbbc155dd8b0a3cab5
Spencer Dearman <dearmanspencer@gmail.com>
2026-03-05 21:14:01 -0600

    very similar to lab 7

--- core/cryptography.py
+++ core/cryptography.py
@@ -1,2 +1,11 @@
 # core/cryptography.py
+from ecdsa import SigningKey, VerifyingKey, SECP256k1
+
+# generates a new SECP256k1 keypair
+def generate_key():
+    sk = SigningKey.generate(curve=SECP256k1)
+    sk_hex = sk.to_string().hex()
+    vk = sk.get_verifying_key()
+    vk_hex = vk.to_string().hex()
+    return sk_hex, vk_hex
 
