d184aec68f7b35aba464f1b0d68022d679ef00cd
Spencer Dearman <dearmanspencer@gmail.com>
2026-03-05 21:43:23 -0600

    added trade to state class, need to now work on the scripting

--- core/state.py
+++ core/state.py
@@ -3,6 +3,10 @@
 from typing import Dict
 from core.account import Account
 
+# for the grid address, I took the alphabet number position of the first letter
+# in each of our CNETs
+GRID_ADDRESS = "0x000000000000000000000000000000034811"
+
 class State:
     def __init__(self):
         self.accounts: Dict[str, Account] = {}
@@ -19,3 +23,24 @@ class State:
         account.micro_coins += coins_delta
         if nonce is not None:
             account.nonce = nonce
+
+    # applies a trade against the grid. push = true (selling) push = false (buying)
+    # might convert push into an enum later
+    def apply_trade(self, user_address: str, energy_amount: int, settlement_amount: int, push: bool, fee: int, miner_address: str):
+        user = self.get_account(user_address)
+        grid = self.get_account(GRID_ADDRESS)
+        miner = self.get_account(miner_address)
+        
+        # user is selling energy to the grid
+        if push:
+            user.energy_wh -= energy_amount
+            user.coins_micro += settlement_amount
+            grid.energy_wh += energy_amount
+            grid.coins_micro -= (settlement_amount + fee)
+        else: # user is buying energy from the grid
+            user.energy_wh += energy_amount
+            user.coins_micro -= (settlement_amount + fee)
+            grid.energy_wh -= energy_amount
+            grid.coins_micro += settlement_amount
+            
+        miner.coins_micro += fee
