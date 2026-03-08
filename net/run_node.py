d0b4e1110431c1523358acfbff6b4eb8d8d374c4
Clarice Kim <claricek@uchicago.edu>
2026-03-08 15:42:02 -0500

    mining impl

--- net/run_node.py
+++ net/run_node.py
@@ -9,14 +9,17 @@ import net.config as config
 import proto.energy_chain_pb2 as energy_chain_pb2
 import proto.energy_chain_pb2_grpc as energy_chain_pb2_grpc
 from core.blockchain import Blockchain
+from core.miner import construct_and_mine_block
 
 # TODO: the following imports will not be necessary after CLI sends txns
 from core.cryptography import generate_key, sign_tx
 from core.block import calculate_tx_hash
 
-# Get port (probably 50051) from config
+# Load values from config
 PORT = config.PORT
 DISCOVERY_TIMEOUT_SECS = config.DISCOVERY_TIMEOUT_SECS
+DIFFICULTY = config.MINING_DIFFICULTY
+MAX_TXN_PER_BLOCK = config.MINING_DIFFICULTY.MAX_TXN_PER_BLOCK # TODO: maybe move this in blockchain class
 
 """Logical representation of the entire node. """
 class Node():
@@ -36,47 +39,12 @@ class Node():
         """
         registration_response = self.register()
         self.discovery(registration_response.last_registered) # Run discovery process
-        self.listening() # This times out after 5 seconds. Assumes all nodes join network in that time period.
+        self.listening_newnodes() # This times out after 5 seconds. Assumes all nodes join network in that time period.
         print(f"Node with address {self.address} starts mining")
         # TODO: Mining
         # TXN discoverer (adds txns to mempool every 2 to 5 secs)
         # Miner 
             # sleeps for some time, starts mining from queue
-
-    # TODO: will be deleted once CLI working
-    def generate_mock_tx(self, priv_key, pub_key, push_rate, pull_rate):
-        """From tests directory."""
-        grid_rate = energy_chain_pb2.GridRateTx()
-        grid_rate.push_rate = push_rate
-        grid_rate.pull_rate = pull_rate
-        grid_rate.expiry_height = 100
-        grid_rate.grid_address = pub_key
-
-        tx = energy_chain_pb2.Transaction()
-        tx.grid_rate_tx.CopyFrom(grid_rate)
-        sign_tx(tx, priv_key)
-        tx.transaction_hash = calculate_tx_hash(tx)
-        return tx
-
-    def discover_txns(self):
-        """For now generates a bunch of transactions and adds to mempool."""
-        priv, pub = generate_key()
-
-        # Generate a lot of initial transactions
-        for _ in range(100):
-            txn = self.generate_mock_tx(priv, pub, 5, 12)
-            self.mempool.put(txn)
-
-        while True:
-            try: 
-                txn = self.generate_mock_tx(priv, pub, 5, 12)
-                self.mempool.put(txn)
-                time.sleep(random.randint(2,5))
-            except KeyboardInterrupt:
-                return
-
-    def mine_loop(self):
-        pass
 
     def register(self):
         """
@@ -119,7 +87,7 @@ class Node():
                 if peer_addr != self.address and (not peer_addr in self.known_peers):
                     self.discovery(peer_addr)
 
-    def listening(self):
+    def listening_newnodes(self):
         """
         Function to listen for new nodes joining the network. Upon a GetPeersRequest (contact by new node), 
         adds new node to list of known peers and sends response of list of known peers. 
@@ -131,6 +99,75 @@ class Node():
         listener.start()
         print("Node server started, listening on " + f"{self.address}:{PORT}")
         listener.wait_for_termination(timeout=DISCOVERY_TIMEOUT_SECS)
+
+    # TODO: will be deleted once CLI working
+    def generate_mock_tx(self, priv_key, pub_key, push_rate, pull_rate):
+        """From tests directory."""
+        grid_rate = energy_chain_pb2.GridRateTx()
+        grid_rate.push_rate = push_rate
+        grid_rate.pull_rate = pull_rate
+        grid_rate.expiry_height = 100
+        grid_rate.grid_address = pub_key
+
+        tx = energy_chain_pb2.Transaction()
+        tx.grid_rate_tx.CopyFrom(grid_rate)
+        sign_tx(tx, priv_key)
+        tx.transaction_hash = calculate_tx_hash(tx)
+        return tx
+
+    def discover_txns(self):
+        """For now generates a bunch of transactions and adds to mempool."""
+        priv, pub = generate_key()
+
+        # Generate a lot of initial transactions
+        for _ in range(100):
+            txn = self.generate_mock_tx(priv, pub, 5, 12)
+            self.mempool.put(txn)
+
+        while True:
+            try: 
+                txn = self.generate_mock_tx(priv, pub, 5, 12)
+                self.mempool.put(txn)
+                time.sleep(random.randint(2,5))
+            except KeyboardInterrupt:
+                print("Shutting down transaction discovery upon keyboard interrupt")
+                return
+
+    def mine_loop(self):
+        # Sleep initially to allow initial transactions to accumulate
+        time.sleep(5)
+
+        while True:
+            try:
+                if not self.mempool.empty():
+                    # Collect transactions
+                    txns_to_mine = []
+                    for _ in range(MAX_TXN_PER_BLOCK):
+                        if not self.mempool.empty():
+                            break
+                        txns_to_mine.append(self.mempool.get())
+
+                    # Attempt to mine block
+                    tip = self.blockchain.get_tip()
+                    mined_block = construct_and_mine_block(tip, txns_to_mine, DIFFICULTY)
+                    if mined_block:
+                        success = self.blockchain.add_block(mined_block)
+                        print(f"Block {mined_block.header.height} Appended: {success}")
+                        print(
+                            f"Block 1 Hash: {mined_block.header.hash_prev_block} (prev) -> Merkle: {mined_block.header.hash_merkle_root}"
+                        )
+                    else:
+                        print("Failed to mine Block")
+                        return
+
+            except KeyboardInterrupt:
+                print("Shutting down mining upon keyboard interrupt")
+                return
+            # TODO: Broadcast the block has been mined / handle that across nodes
+
+            # Upon finishing, sleep for random amount of time to avoid conflicts
+            time.sleep(random.randint(3,5))
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
